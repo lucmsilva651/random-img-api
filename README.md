70be6a09e36b555dab1863fe57f9e5a2a97cb83b
Luquinhas <lucmsilva651@gmail.com>
2025-03-24 23:49:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Random Image API
+A Node.js API that serves random images.
+
+## Installation
+- Clone the repository with Git or download the source.
+- Put your desired images on ``images`` inside the ``src`` folder.
+- Install all project dependencies with ``npm i`` or ``bun i``.
+- Run the server with ``npm start`` or ``bun start``.
+
+## Development: Personal tip
+While testing, you can use ``nodemon`` to avoid restarting manually the server whenever you changed something.
+- Install ``nodemon`` with ``npm i nodemon -g`` or ``bun i nodemon -g``
+- Then run it with just ``nodemon``.
+With this, you can avoid restarting your server manually. Do not worry. ``nodemon`` restarts the server whenever you changed some file.
+
+## Notes
+Please read this carefully if you are really going to use this:
+- Images are being served on a 1:1 aspect ratio, with the resolution being 400x400 by default. Feel free to change this if you want.
+- Ignore the sample images. These are only meant to be on my personal README.
+
+## License
+2025 Lucas Gabriel (lucmsilva) - BSD 3-Clause
---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const sharp = require("sharp");
+const app = express();
+const port = process.env.PORT || 3000;
+const imagesDir = path.join(__dirname, "images");
+let lastImage = null;
+
+app.get("/", async (req, res) => {
+  fs.readdir(imagesDir, async (err, files) => {
+    if (err || files.length === 0) {
+      return res.status(404).json({ error: "No image found on /images. Did you forget to add your images there?" });
+    }
+
+    let randomImage;
+    do {
+      randomImage = files[Math.floor(Math.random() * files.length)];
+    } while (files.length > 1 && randomImage === lastImage);
+
+    lastImage = randomImage;
+    const imagePath = path.join(imagesDir, randomImage);
+
+    try {
+      const imageBuffer = await sharp(imagePath)
+        .resize(450, 450, { fit: "cover" })
+        .toBuffer();
+
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.end(imageBuffer);
+    } catch (error) {
+      res.status(500).json({ error: "Error processing image." });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
